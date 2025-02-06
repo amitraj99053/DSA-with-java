61b4572782d1b155f7bc676941f78c5782b9cede
AMIT KUMAR <113933332+amitraj99053@users.noreply.github.com>
2025-02-07 00:00:21 +0530

    Add files via upload

--- Pattern10.java
+++ Pattern10.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+// butterfly pattern
 
 public class Pattern10 {
     public static void main(String[] args) {
@@ -6,6 +7,45 @@ public class Pattern10 {
         System.out.print("Enter no: ");
         int n = in.nextInt();
 
-        
+        // upper half
+        for (int i=1; i<=n; i++){
+            //1st part
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            // space
+            int spaces = 2*(n-i);
+            for (int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+
+            // 2nd part
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower part
+        for (int i=n; i>=1; i--){
+            //1st part
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            // space
+            int spaces = 2*(n-i);
+            for (int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+
+            // 2nd part
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
