c6a38219dcbcfeeb5d40d5eda0be45d213b67d20
AMIT KUMAR <113933332+amitraj99053@users.noreply.github.com>
2025-02-28 19:24:45 +0530

    Add files via upload

--- leetCodeQn1.java
+++ leetCodeQn1.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+import java.util.HashMap;
+// roman number to integer number (conversion)
 public class leetCodeQn1 {
-
 
         public int romanToInt(String s) {
             if(s == null || s.length() == 0){
@@ -32,6 +34,14 @@ public class leetCodeQn1 {
             return result;
 
         }
-        
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a random numeral: ");
+        String ramanNumeral = in.nextLine();
+        leetCodeQn1 solution = new leetCodeQn1();
+        int integerValue = solution.romanToInt(ramanNumeral);
+        System.out.println("Integer value: "+ integerValue);
+        in.close();
+    }
 }
