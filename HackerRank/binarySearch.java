2b811f959ac74d9fb070f56c6e412b34031a4099
amitraj99053 <amitraj99053@gmail.com>
2025-03-17 21:28:41 +0530

    Update

--- HackerRank/binarySearch.java
+++ HackerRank/binarySearch.java
@@ -1,26 +1,26 @@
 public class binarySearch {
     // Binary search
     public static int binarysearch(int[] arr, int k) {
-        
-        int start = 0, end = arr.length-1;
-            
-        while(start <= end) {
-        int mid = (start + end) / 2;
-                
-        if(arr[mid] == k) {
-        return mid;
+        // Code Here
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == k) {
+                result = mid; 
+                right = mid - 1;
+            } else if (arr[mid] < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
 
-        if(arr[mid] < k) {
-        start = mid+1;
-        } else {
-        end = mid-1;
-        }
-
-        }
-            
-        return -1;
+        return result;
     }
+    
     
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5};
