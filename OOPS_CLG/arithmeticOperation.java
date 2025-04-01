dbf348d12e5b335c61c8ef82e798c153196bbe6b
amitraj99053 <amitraj99053@gmail.com>
2025-04-01 21:36:16 +0530

    Update

---
+++ OOPS_CLG/arithmeticOperation.java
@@ -0,0 +1,18 @@
+
+public class arithmeticOperation {
+    public static void main(String[] args) {
+        // arithmetic operation
+        int num1 = 16, num2 = 3;
+
+        System.out.println("Sum: "+(num1+num2));
+        System.out.println("Sub: "+(num1-num2));
+        System.out.println("Mul: "+(num1*num2));
+        System.out.println("Div: "+(num1/num2));
+        System.out.println("Mod: "+(num1%num2));
+        System.out.println(" ");
+        System.out.println(-5+8*6);
+        System.out.println((55+9)%9);
+        System.out.println(20+-3*5/8);
+        System.out.println(5+15/3*2-8%3);
+    }
+}
