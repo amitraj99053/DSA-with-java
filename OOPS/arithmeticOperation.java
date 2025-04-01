fed012d041ae70f9bb0ce87d58210a7ef211fd73
amitraj99053 <amitraj99053@gmail.com>
2025-04-01 22:06:46 +0530

    Update

--- OOPS/arithmeticOperation.java
+++ OOPS/arithmeticOperation.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class arithmeticOperation {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         // arithmetic operation
-        int num1 = 16, num2 = 3;
+        System.out.print("Enter two no.s: ");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
 
         System.out.println("Sum: "+(num1+num2));
         System.out.println("Sub: "+(num1-num2));
@@ -13,5 +18,7 @@ public class arithmeticOperation {
         System.out.println((55+9)%9);
         System.out.println(20+-3*5/8);
         System.out.println(5+15/3*2-8%3);
+
+        in.close();
     }
 }
--- OOPS/compareTwoNo.java
+++ OOPS/compareTwoNo.java
@@ -1,7 +1,16 @@
-package OOPS;
+import java.util.Scanner;
 
 public class compareTwoNo {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two no.s: ");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+
+        if(num1 > num2) {
+            System.out.println(num1);
+        } else {
+            System.out.println(num2);
+        }
     }
 }
