13eba79c77bf7463b06a7b794d4e33b2d78564ad
Amit Kumar <amitraj99053@gmail.com>
2025-04-14 23:10:00 +0530

    Upload

--- OOPS-CLG/CheckNoIsPositive.java
+++ OOPS-CLG/CheckNoIsPositive.java
@@ -4,7 +4,7 @@ public class CheckNoIsPositive {
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
 
-        if(num > 0) {
+        if(num >= 0) {
             System.out.println("Positive no: "+num);
         } else {
             System.out.println("Negative no: "+num);
