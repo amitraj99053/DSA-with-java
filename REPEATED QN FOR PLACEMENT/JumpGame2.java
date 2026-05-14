e6b6e155f1074df2b6645076fa52f4f00cc3d3a0
AMIT KUMAR <113933332+amitraj99053@users.noreply.github.com>
2026-05-14 21:10:03 +0530

    Jump game 2

---
+++ REPEATED QN FOR PLACEMENT/JumpGame2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int ans = 0; 
+        int mx = 0;  
+        int last = 0; 
+
+        for (int i = 0; i < nums.length - 1; ++i) {
+            
+            mx = Math.max(mx, i + nums[i]);
+            
+        
+            if (last == i) {
+                ++ans; 
+                last = mx; 
+            }
+        }
+        return ans;
+    }
+}
